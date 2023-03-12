b1ec823654c1051bb8c56d4d9a1684e9d75e2e5f
Antonio Peixe <antonio.icr.peixe@gmail.com>
2023-03-12 19:36:33 +0000

    refactor(lexer): use more iterators
      - implement Iterator for Lexer
      - implement From for Lexer
      - use iterators when reading program

--- src/lexer.rs
+++ src/lexer.rs
@@ -21,114 +21,128 @@ enum Token {
     LET,
 }
 
-fn is_letter(ch: Option<char>) -> bool {
-    match ch {
-        None => false,
-        Some(x) => { x.is_ascii_alphabetic() || x == '_' },
-    }
+fn is_letter(ch: char) -> bool {
+    // allowing `_` for identifiers
+    return ch.is_ascii_alphabetic() || ch == '_';
 }
 
-fn is_digit(ch: Option<char>) -> bool {
-    match ch {
-        None => false,
-        Some(x) => { x.is_numeric() },
-    }
+fn is_digit(ch: char) -> bool {
+    return ch.is_numeric();
 }
 
-fn is_whitespace(ch: Option<char>) -> bool {
-    match ch {
-        None => false,
-        Some(x) => { x == ' ' || x == '\t' || x == '\n' || x == '\r' },
-    }
+fn is_whitespace(ch: char) -> bool {
+    return ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r';
 }
 
-fn look_up_identifier(ident: &str) -> Token {
-    match ident {
+fn look_up_identifier(ident: String) -> Token {
+    return match ident.as_str() {
         "fn" => Token::FUNCTION,
         "let" => Token::LET,
-        _ => Token::IDENTIFIER(ident.to_string()),
-    }
+        _ => Token::IDENTIFIER(ident),
+    };
 }
 
 struct Lexer {
     input: String,
-    position: usize,
     read_position: usize,
     ch: Option<char>,
 }
 
-fn build_lexer(input: String) -> Lexer {
-    let mut l = Lexer { input, position: 0, read_position: 0, ch: None };
-    l.read_char();
-    l
+impl From<String> for Lexer {
+    fn from(string: String) -> Self {
+        let mut l = Lexer {
+            input: string,
+            read_position: 0,
+            ch: None,
+        };
+        l.read_char();
+        return l;
+    }
+}
+
+impl Iterator for Lexer {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.skip_white_spaces();
+        let token: Token = match self.ch {
+            None => Token::EOF,
+            Some(x) => match x {
+                ',' => Token::LIMITER(LimiterToken::COMMA),
+                ';' => Token::LIMITER(LimiterToken::SEMICOLON),
+                '(' => Token::LIMITER(LimiterToken::LPAREN),
+                ')' => Token::LIMITER(LimiterToken::RPAREN),
+                '{' => Token::LIMITER(LimiterToken::LBRACE),
+                '}' => Token::LIMITER(LimiterToken::RBRACE),
+                '+' => Token::PLUS,
+                '=' => Token::ASSIGN,
+                _ => {
+                    if is_letter(x) {
+                        look_up_identifier(self.read_identifier())
+                    } else if is_digit(x) {
+                        Token::LITERAL(self.read_number().to_string())
+                    } else {
+                        Token::ILLEGAL
+                    }
+                }
+            },
+        };
+        self.increment_read_position(&token);
+        self.read_char();
+        return Some(token);
+    }
 }
 
 impl Lexer {
     fn read_char(&mut self) {
-        if self.read_position >= self.input.len() {
-            self.ch = None;
-        } else {
-            // FIXME: it's probably quite bad to convert the whole string every time
-            self.ch = Some(char::from(self.input.as_bytes()[self.read_position]));
-        }
-        self.position = self.read_position;
-        self.read_position += 1;
+        // FIXME: it's probably bad to create the iterator everytime
+        self.ch = self.input.chars().nth(self.read_position);
     }
 
-    fn read_identifier(&mut self) -> &str {
-        let pos = self.position;
-        while is_letter(self.ch) {
-            self.read_char();
+    fn increment_read_position(&mut self, token: &Token) {
+        match token {
+            Token::ILLEGAL | Token::EOF => (),
+            Token::IDENTIFIER(s) | Token::LITERAL(s) => self.read_position += s.len(),
+            Token::LIMITER(_) | Token::ASSIGN | Token::PLUS => self.read_position += 1,
+            Token::FUNCTION => self.read_position += 2,
+            Token::LET => self.read_position += 3,
         }
-        &self.input[pos..self.position]
     }
 
-    fn read_number(&mut self) -> &str {
-        let pos = self.position;
-        while is_digit(self.ch) {
-            self.read_char();
-        }
-        &self.input[pos..self.position]
+    fn read_identifier(&self) -> String {
+        return self
+            .input
+            .chars()
+            .skip(self.read_position)
+            .take_while(|ch| is_letter(*ch))
+            .collect::<String>();
+    }
+
+    fn read_number(&self) -> String {
+        return self
+            .input
+            .chars()
+            .skip(self.read_position)
+            .take_while(|ch| is_digit(*ch))
+            .collect::<String>();
     }
 
     fn skip_white_spaces(&mut self) {
-        while is_whitespace(self.ch) { self.read_char(); }
-    }
+        let spaces = self
+            .input
+            .chars()
+            .skip(self.read_position)
+            .take_while(|c| is_whitespace(*c))
+            .collect::<String>();
 
-    fn next_token(&mut self) -> Token {
-        self.skip_white_spaces();
-        let t: Token = match self.ch {
-            None => Token::EOF,
-            Some(x) => match x {
-                    ',' => Token::LIMITER(LimiterToken::COMMA),
-                    ';' => Token::LIMITER(LimiterToken::SEMICOLON),
-                    '(' => Token::LIMITER(LimiterToken::LPAREN),
-                    ')' => Token::LIMITER(LimiterToken::RPAREN),
-                    '{' => Token::LIMITER(LimiterToken::LBRACE),
-                    '}' => Token::LIMITER(LimiterToken::RBRACE),
-                    '+' => Token::PLUS,
-                    '=' => Token::ASSIGN,
-                    _ => {
-                        // early return because we can't run `self.read_char` at the end, we
-                        // already incremented during `read_identifier`
-                        if is_letter(Some(x)) {
-                            return look_up_identifier(self.read_identifier());
-                        } else if is_digit(Some(x)) {
-                            return Token::LITERAL(self.read_number().to_string());
-                        } else {
-                            Token::ILLEGAL
-                        }
-                    },
-            }
-        };
+        // spaces don't create a token so we much increment and re-read the next char
+        self.read_position += spaces.len();
         self.read_char();
-        t
     }
 }
 
 #[test]
 fn lexer_test() {
-
     let program = "let five = 5;
     let ten = 10;
     let add = fn(x, y) {
@@ -137,21 +151,19 @@ fn lexer_test() {
     let result = add(five, ten);
     ";
     let input = String::from(program);
-    let mut lex = build_lexer(input);
-    
-    let tests: [Token; 37]  = [
+    let lex = Lexer::from(input);
+
+    let tests: [Token; 37] = [
         Token::LET,
         Token::IDENTIFIER(String::from("five")),
         Token::ASSIGN,
         Token::LITERAL(String::from("5")),
         Token::LIMITER(LimiterToken::SEMICOLON),
-
         Token::LET,
         Token::IDENTIFIER(String::from("ten")),
         Token::ASSIGN,
         Token::LITERAL(String::from("10")),
         Token::LIMITER(LimiterToken::SEMICOLON),
-
         Token::LET,
         Token::IDENTIFIER(String::from("add")),
         Token::ASSIGN,
@@ -168,7 +180,6 @@ fn lexer_test() {
         Token::LIMITER(LimiterToken::SEMICOLON),
         Token::LIMITER(LimiterToken::RBRACE),
         Token::LIMITER(LimiterToken::SEMICOLON),
-
         Token::LET,
         Token::IDENTIFIER(String::from("result")),
         Token::ASSIGN,
@@ -179,13 +190,13 @@ fn lexer_test() {
         Token::IDENTIFIER(String::from("ten")),
         Token::LIMITER(LimiterToken::RPAREN),
         Token::LIMITER(LimiterToken::SEMICOLON),
-
         Token::EOF,
-
     ];
 
-    for test_token in tests.iter() {
-        let token: Token = lex.next_token();
-        assert_eq!(token, *test_token);
-    };
+    lex.into_iter()
+        .zip(tests.iter())
+        .map(|(token, test_token)| {
+            assert_eq!(token, *test_token);
+        })
+        .for_each(drop);
 }
